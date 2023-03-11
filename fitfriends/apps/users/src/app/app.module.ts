d4025ef3fb477503df1792b07640794f7825ecd0
Elena <eamatytsina@gmail.com>
2023-03-11 18:30:51 +0300

    подключен ConfigModule

--- fitfriends/apps/users/src/app/app.module.ts
+++ fitfriends/apps/users/src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { FitUserModule } from './fit-user/fit-user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from '../config/database.config';
 
 @Module({
-  imports: [FitUserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal:true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig]
+    }),
+    FitUserModule,
+    AuthModule],
   controllers: [],
   providers: [],
 })
