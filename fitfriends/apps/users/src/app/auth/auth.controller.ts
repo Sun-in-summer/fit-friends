7ded8d6a87e83528479476b9257a3404a2fca7df
Elena <eamatytsina@gmail.com>
2023-03-11 17:39:33 +0300

    добавлены обработчики  маршрутов 'auth/register', `auth/login`, `auth/:id`

--- fitfriends/apps/users/src/app/auth/auth.controller.ts
+++ fitfriends/apps/users/src/app/auth/auth.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { fillObject } from '@fitfriends/core';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
+import { UserRdo } from './rdo/user.rdo';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) {}
+
+  @Post('register')
+  async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillObject(UserRdo, newUser);
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillObject(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string){
+    const existUser= await this.authService.getUser(id);
+    return fillObject(UserRdo, existUser);
+  }
+}
