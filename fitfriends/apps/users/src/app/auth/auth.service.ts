3a7153f0b88ddb600d2dd77d6821e8cbad936d4c
Elena <eamatytsina@gmail.com>
2023-03-11 16:49:22 +0300

    добавлены методы verifyUser(проверка логина и пароля) и getUser в AuthService

--- fitfriends/apps/users/src/app/auth/auth.service.ts
+++ fitfriends/apps/users/src/app/auth/auth.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 import dayjs from 'dayjs';
 import { FitUserMemoryRepository } from '../fit-user/fit-user-memory.repository';
 import { FitUserEntity } from '../fit-user/fit-user.entity';
-import { AUTH_USER_EXISTS } from './auth.constant';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './auth.constant';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -36,5 +37,28 @@ export class AuthService {
 
     return this.fitUserRepository.create(userEntity);
   }
+
+  async verifyUser(dto:LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.fitUserRepository.findByEmail(email);
+
+    if(!existUser) {
+      throw new Error(AUTH_USER_NOT_FOUND);
+    }
+
+    const fitUserEntity = new FitUserEntity(existUser);
+    if (!await fitUserEntity.comparePassword(password)){
+      throw new Error (AUTH_USER_PASSWORD_WRONG)
+    }
+
+    return fitUserEntity.toObject();
+  }
+
+
+  async getUser(id: string) {
+    return this.fitUserRepository.findById(id);
+  }
+
+
 }
 
