cc43ebd1abd241f5c4fedf7768edabb843f0a8f7
Elena <eamatytsina@gmail.com>
2023-03-11 19:36:16 +0300

    добавлена валидация переменных окружения

--- fitfriends/apps/users/src/app/app.module.ts
+++ fitfriends/apps/users/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_FILE_PATH } from './app.constant';
 import databaseConfig from '../config/database.config';
+import { validateEnvironments } from './env.validation';
+
 
 @Module({
   imports: [
@@ -11,7 +13,8 @@ import databaseConfig from '../config/database.config';
       cache: true,
       isGlobal:true,
       envFilePath: ENV_FILE_PATH,
-      load: [databaseConfig]
+      load: [databaseConfig],
+      validate: validateEnvironments,
     }),
     FitUserModule,
     AuthModule],
