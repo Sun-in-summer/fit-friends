c04b39237272490f9991aee302283c9133b14f8c
Elena <eamatytsina@gmail.com>
2023-03-11 11:23:03 +0300

    реэкспорт из shared -types

--- fitfriends/libs/shared-types/src/index.ts
+++ fitfriends/libs/shared-types/src/index.ts
@@ -1,1 +1,13 @@
+export *  from './lib/user.interface';
+export *  from './lib/user-role.enum';
+export *  from './lib/training.interface';
+export *  from './lib/personal-training-order.interface';
+export *  from './lib/gym.interface';
+export *  from './lib/location.enum';
+export *  from './lib/comment.interface';
+export *  from './lib/gender.enum';
+export *  from './lib/notification.interface';
+export *  from './lib/order.interface';
+
+
 
