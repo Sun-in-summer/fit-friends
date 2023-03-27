bbc1da977e7f220e26ad98052f393bd1329d31a9
Elena <eamatytsina@gmail.com>
2023-03-27 16:02:58 +0300

    minor fixes

--- fitfriends/apps/training/src/app/guards/roles.guard.ts
+++ fitfriends/apps/training/src/app/guards/roles.guard.ts
@@ -25,12 +25,7 @@ export class RolesGuard implements CanActivate {
 
     if (role) {
 
-
-
       const request = context.switchToHttp().getRequest();
-
-      console.log(request);
-
       const user = request.user;
 
       console.log({...user});
