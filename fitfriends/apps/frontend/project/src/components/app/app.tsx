5ef252b2f9b50d45fbe6645dfba642f6f4947b8a
Elena <eamatytsina@gmail.com>
2023-05-05 18:48:58 +0300

    [frontend]: добавлены страницы intro, signIn, signUp

---
+++ fitfriends/apps/frontend/project/src/components/app/app.tsx
@@ -0,0 +1,13 @@
+// import IntroScreen from '../../pages/intro-screen/intro-screen';
+// import SignInScreen from '../../pages/sign-in/sing-in';
+import SignUpScreen from '../../pages/sign-up/sign-up';
+
+function App(): JSX.Element {
+  return (
+    // <IntroScreen/>
+    // <SignInScreen/>
+    <SignUpScreen/>
+  );
+}
+
+export default App;
